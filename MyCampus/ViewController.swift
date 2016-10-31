f7a352f888171fc7414b820b84a20770cc2eebb2
Jon Corbett <corbett.jo@husky.neu.edu>
2016-10-31 15:26:42 -0400

    Initial Commit

---
+++ MyCampus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCampus
+//
+//  Created by Jonathan Corbett on 10/31/16.
+//  Copyright © 2016 HCI Dream Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
