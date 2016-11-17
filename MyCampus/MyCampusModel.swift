8a5dc6a242424af694052a919fe104e4daae015a
baharsheikhi <haji-sheikhi.b@husky.neu.edu>
2016-11-16 20:51:21 -0500

    added search and it WORKS!

---
+++ MyCampus/MyCampusModel.swift
@@ -0,0 +1,33 @@
+//
+//  MyCampusModel.swift
+//  MyCampus
+//
+//  Created by Bahar Sheikhi on 11/16/16.
+//  Copyright © 2016 HCI Dream Team. All rights reserved.
+//
+
+import Foundation
+
+class MyCamusModel {
+    //MARK: Properties 
+    var events = [Event]()
+    
+    let carseatHeadrest = Event(name: "Carseat Headrest", photo: "carseat", price: "$7.00", day: 23, month: "November", description: "Come hear this American indie rock band originally from Leesburg, Virginia. They are currently based out of Seattle, Washington.", location: "AfterHours", time: "7:00pm", icon: "concert")
+    
+     let csa = Event(name: "Chinese Student Association Info Session", photo: "csa", price:"free!", day: 25, month:"November", description:"Come meet the members of our club and learn about our events.", location:"Curry Student Center", time:"6:00pm", icon:"talk")
+    
+     let hockeyBU = Event(name: "NU Hockey vs BU", photo: "hockey", price:"free for students!", day: 29, month:"October", description:"Come join your fellow huckies root against the Terrires in the dog house! It'll be paws-itively awesome! :-)", location:"Matthew's Arena", time:"7:00pm", icon:"sport")
+    
+    
+    init() {
+        events.append(carseatHeadrest)
+        events.append(csa)
+        events.append(hockeyBU)
+    }
+    
+    public func getEvents() -> [Event] {
+        return events
+    }
+    
+    
+}
---
+++ MyCampus/SearchViewController.swift
@@ -0,0 +1,101 @@
+//
+//  SearchViewController.swift
+//  MyCampus
+//
+//  Created by Bahar Sheikhi on 11/15/16.
+//  Copyright © 2016 HCI Dream Team. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var tbleSearchResults: UITableView!
+    var model = MyCamusModel()
+    var dataArray = [Event]()
+    var filteredArray = [Event]()
+    var shouldShowSearchResults = false
+    var searchController = UISearchController(searchResultsController: nil)
+    
+    func loadListOfEvents() {
+        dataArray = model.getEvents()
+        
+        tbleSearchResults.reloadData()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tbleSearchResults.delegate = self
+        tbleSearchResults.dataSource = self
+        loadListOfEvents()
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        tbleSearchResults.tableHeaderView = searchController.searchBar
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+//    override func prepareForSegue(_:sender:) {
+//        let event: Event
+//        if searchController.isActive && searchController.searchBar.text != "" {
+//            candy = filteredCandies[indexPath.row]
+//        } else {
+//            candy = candies[indexPath.row]
+//        }
+//    }
+    
+    // MARK: UITableView Delegate and Datasource functions
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func filterContentForSearchText(searchText: String, scope: String = "All") {
+        filteredArray = dataArray.filter { event in
+            return (event.name.lowercased().range(of: searchText.lowercased()) != nil)
+        }
+        
+        tbleSearchResults.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        if searchController.isActive && searchController.searchBar.text != "" {
+            return filteredArray.count
+        }
+        return dataArray.count
+    }
+    
+
+      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
+        let candy: Event
+        if searchController.isActive && searchController.searchBar.text != "" {
+            candy = filteredArray[indexPath.row]
+        } else {
+            candy = dataArray[indexPath.row]
+        }
+        cell.textLabel?.text = candy.name
+        cell.detailTextLabel?.text = candy.description
+        return cell
+    }
+    
+    
+    func configureSearchController() {
+        searchController = UISearchController(searchResultsController: nil)
+    }
+
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchText: searchController.searchBar.text!)
+    }
+}
