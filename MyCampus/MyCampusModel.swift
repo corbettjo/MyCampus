5ebe89d4f4de2275b384d4c27fbf869c8d6787db
baharsheikhi <haji-sheikhi.b@husky.neu.edu>
2016-11-18 23:21:08 -0500

    most stable version

--- MyCampus/MyCampusModel.swift
+++ MyCampus/MyCampusModel.swift
@@ -10,15 +10,18 @@ import Foundation
 
 
 var modelDemo = MyCampusModel();
+
+var category = ""
+
 var selectedEvent = Event(name: "NIL VERSION: Carseat Headrest", photo: "carseat", price: "$7.00", day: 23, month: "November", description: "Come hear this American indie rock band originally from Leesburg, Virginia. They are currently based out of Seattle, Washington.", location: "AfterHours", time: "7:00pm", icon: ["concert"])
 
 class MyCampusModel {
-    //MARK: Properties 
+    //MARK: Properties
     var events = [Event]()
     var going = [Event]()
     
     let carseatHeadrest = Event(name: "Carseat Headrest", photo: "carseat", price: "$7.00", day: 23, month: "November", description: "Come hear this American indie rock band originally from Leesburg, Virginia. They are currently based out of Seattle, Washington.", location: "AfterHours", time: "6:30pm", icon: ["music"])
-
+    
     
     let csa = Event(name: "Chinese Student Association Info Session", photo: "csa", price:"free!", day: 25, month:"November", description:"Come meet the members of our club and learn about our events.", location:"Curry Student Center", time:"6:00pm", icon: ["clubs", "free"])
     
@@ -32,7 +35,7 @@ class MyCampusModel {
     
     let basketball = Event(name: "NU Basketball vs BU", photo: "basketball", price: "free for students!", day: 2, month: "February", description: "Come join your fellow huskies root agains the Wildcats in the Dog House! It'll be paws-itively awesome! :)", location: "Matthews Arena", time: "7:00pm", icon: ["sports", "free"])
     
-    let scout = Event(name: "Meet Scout", photo: "scout", price: "free for students!", day: 7, month: "February", description: "Interested in design and development? Meet the members of Scout!", location: "Ryder 375", time: "8:00", icon: ["clubs", "free"])
+    let scout = Event(name: "Meet Scout", photo: "scout", price: "free for students!", day: 7, month: "February", description: "Interested in design and development? Meet the members of Scout!", location: "Ryder 375", time: "8:00", icon: ["club", "free"])
     
     let volunteer = Event(name: "Volunteer with Northeastern", photo: "volunteer", price: "free", day: 12, month: "December", description: "Take a break from finals and volunteer during the Christmas season", location: "Centennial Quad", time: "8:00", icon: ["community", "free"])
     
@@ -44,7 +47,7 @@ class MyCampusModel {
     
     let strange = Event(name: "Viewing of Doctor Strange", photo: "strange", price: "free for students!", day: 22, month: "November", description: "Watch Doctor Strange in Centennial Quad", location: "Centennial Quad", time: "8:00", icon: ["video", "free"])
     
-    let art = Event(name: "Painting with RSA", photo: "rsa", price: "free for students!", day: 18, month: "November", description: "Bring a friend and compete for the best landscape painting!", location: "Ryder 403", time: "5:00", icon: ["fineArts", "free"])
+    let art = Event(name: "Painting with RSA", photo: "rsa", price: "free for students!", day: 18, month: "November", description: "Bring a friend and compete for the best landscape painting!", location: "Ryder 403", time: "5:00", icon: ["free", "fineArts"])
     
     init() {
         events.append(carseatHeadrest)
@@ -80,5 +83,18 @@ class MyCampusModel {
         else {
             let indexRemove = going.index(of: goEvent);
             going.remove(at: indexRemove!); }
+    }
+    
+    public func getEvents(ofCategory: String) -> [Event] {
+        var matches = [Event]()
+        for e in self.getEvents() {
+            for s in e.icon {
+                if s == ofCategory {
+                    matches.append(e)
+                }
+            }
+        }
+        
+        return matches
     }
 }
